17e9c2a097097bdfcc540cf91a75603818ada733
Michael Graf <mgraf@hotmail.ch>
2023-09-20 02:58:57 +0200

    Added documentation for x_get_xcb_connection.

--- src/lib.rs
+++ src/lib.rs
@@ -15,6 +15,20 @@ struct Display
     _private: *mut xlib::cdef::Display
 }
 
+/// Returns the XCB connection associated with an Xlib Display.
+///
+/// # Parameters
+/// ## display
+/// Specifies the connection to the X server.
+///
+/// # Return value
+/// The XCB connection associated with the given Xlib Display.
+///
+/// # Remarks
+/// The XCB connection returned by x_get_xcb_connection can be used with functions
+/// from the XCB library, in the same way as an XCB connection created with XCB.
+/// However, it probably is a good idea to use x_close_display instead of
+/// xcb_disconnect to disconnect from the X server.
 pub fn x_get_xcb_connection(display: &xlib::Display) -> xcb::XCBConnection
 {
     let connection = unsafe {
